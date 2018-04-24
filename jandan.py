76dd3e0a4d7a2d9e8c89656eded255bf38fde04a
jimmyczm <35089008+jimmyczm@users.noreply.github.com>
2018-04-24 11:15:44 +0800

    Update jandan.py

--- jandan.py
+++ jandan.py
@@ -7,9 +7,9 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from urllib.parse import quote
-from pyquery import  PyQuery as pq
+from bs4 import BeautifulSoup as bs
 import pymongo
+import re
 import time
 
 
@@ -17,13 +17,14 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(chrome_options=chrome_options)
+wait = WebDriverWait(browser,10)
 
-MAX_PAGE=5
+MAX_PAGE=100  #抓取的页面数
 
 MONGO_URL = 'localhost'
 MONGO_DB ='jandan'
-MONGO_COLLECTION = 'duanzi0419'
+MONGO_COLLECTION = 'duanzi0423'
 client = pymongo.MongoClient(MONGO_URL)
 db = client[MONGO_DB]
 
@@ -32,24 +33,26 @@ def get_page (page):
     url = 'http://jandan.net/duan/page-'+str(page)+'#comments'
     try:
         browser.get(url)
-        #print(browser.page_source)
-        time.sleep(2)
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'.commentlist ')))
+        time.sleep(20)
         get_texts()
-        print('正在获取源代码')
+        print('正在获取页面')
     except TimeoutException:
         get_page (page)
     
 def get_texts ():
 #获取段子内容，id,ooxx数
     html = browser.page_source
-    doc = pq(html)
-    items = doc('li').items()
+    doc = bs(html,'lxml')
+    items = doc(id = re.compile('comment-.*'))
     for item in items :
+        if item('bad_content'):
+            continue
         text ={
-        'id':item.find('href').text(),
-        'duanzi': item.find('p').text(),
-        'oo': item.find('tucao-like-container').text(),
-        'xx': item.find('tucao-unlike-container').text()
+            'text':repr(item.p.string).replace('\\r\\n','').replace(' ','').replace('\'',''),
+            'oo':int(item.find(class_ = 'tucao-like-container').span.get_text(strip=True)),
+            'xx':int(item.find(class_ = 'tucao-unlike-container').span.get_text(strip=True))
+
         }
         print('正在保存',text)
         save(text)
@@ -64,7 +67,7 @@ def save (result):
         
        
 def main ():
-#需要遍历的页面
+#遍历页面
     for i  in range(1,MAX_PAGE+1):
         get_page(i)
     browser.close()
